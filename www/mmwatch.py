e16856d63861d0d672d667d55635d39cadd5c954
Ilya Zverev <zverik@textual.ru>
2016-03-09 00:54:50 +0300

    styling and filters

--- www/mmwatch.py
+++ www/mmwatch.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 import os, json, peewee
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, url_for
 from flask.ext.compress import Compress
 import config
 
@@ -23,6 +23,22 @@ class Change(peewee.Model):
   address = peewee.BooleanField(default=False)
   changes = peewee.TextField()
 
+  def changed_coord(self):
+    return json.loads(self.changes)[0]
+
+  def changed_tags(self):
+    tags = json.loads(self.changes)[1]
+    for t in tags:
+      if tags[t][0] is None:
+        tags[t].append('create')
+      elif tags[t][1] is None:
+        tags[t].append('delete')
+      elif tags[t][0] != tags[t][1]:
+        tags[t].append('modify')
+      else:
+        tags[t].append('nothing')
+    return tags
+
   class Meta:
     database = database
     db_table = 'mapsme_change'
@@ -36,13 +52,38 @@ def teardown(exception):
   if not database.is_closed():
     database.close()
 
+def purl(params, **kwargs):
+  p2 = params.copy()
+  for k, v in kwargs.iteritems():
+    p2[k] = v
+  return url_for('the_one_and_only_page', **p2)
+
 @app.route('/')
 def the_one_and_only_page():
-  changes = Change.select().order_by(Change.id.desc()).paginate(1, config.PAGE_SIZE)
+  # Parse query params
+  params = {}
+  page = request.args.get('page', '1')
+  params['page'] = int(page) if page.isdigit() else 1
+  user = request.args.get('user', None)
+  if user is not None:
+    params['user'] = user
+
+  # Construct queries
+  changes = Change.select().order_by(Change.id.desc()).paginate(params['page'], config.PAGE_SIZE)
   users = Change.select(Change.user, peewee.fn.Count(Change.id).alias('count')).group_by(Change.user).order_by(peewee.fn.Count(Change.id).desc()).limit(config.TOP)
   tags = Change.select(Change.main_tag, peewee.fn.Count(Change.id).alias('count')).group_by(Change.main_tag).order_by(peewee.fn.Count(Change.id).desc()).limit(config.TOP)
   versions = Change.select(Change.version, peewee.fn.Count(Change.id).alias('count')).group_by(Change.version).order_by(peewee.fn.Count(Change.id).desc()).limit(config.TOP)
-  stat_src = Change.select(Change.action, Change.obj_type, peewee.fn.Count(Change.id).alias('count')).group_by(Change.action, Change.obj_type).order_by(peewee.fn.Count(Change.id).desc()).limit(config.TOP)
+  stat_src = Change.select(Change.action, Change.obj_type, peewee.fn.Count(Change.id).alias('count')).group_by(Change.action, Change.obj_type).order_by(peewee.fn.Count(Change.id).desc())
+
+  # Apply filters
+  if user:
+    changes = changes.where(Change.user == user)
+    users = users.where(Change.user == user)
+    tags = tags.where(Change.user == user)
+    versions = versions.where(Change.user == user)
+    stat_src = stat_src.where(Change.user == user)
+
+  # Calculate statistics
   stats = {}
   stats['created'] = stats['deleted'] = stats['modified'] = stats['anomalies'] = 0
   stats['nodes'] = stats['ways'] = stats['relations'] = stats['total'] = 0
@@ -62,8 +103,9 @@ def the_one_and_only_page():
       stats['ways'] += stat.count
     elif stat.obj_type == 'r':
       stats['relations'] += stat.count
+  stats['pages'] = (stats['total'] + config.PAGE_SIZE - 1) / config.PAGE_SIZE
 
-  return render_template('index.html', stats=stats, changes=changes, users=users, tags=tags, versions=versions)
+  return render_template('index.html', stats=stats, changes=changes, users=users, tags=tags, versions=versions, params=params, purl=purl)
 
 if __name__ == '__main__':
   app.run(threaded=True)
