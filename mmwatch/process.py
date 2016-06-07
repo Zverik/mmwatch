6b3d482f548a743e51887487148ccc7e760c8d3c
Ilya Zverev <zverik@textual.ru>
2016-06-07 18:36:09 +0300

    Remove notes for a time

--- mmwatch/process.py
+++ mmwatch/process.py
@@ -1,7 +1,3 @@
 #!/usr/bin/env python
 from server import mapsme_process
-from server import parse_notes
-print 'Processing changes'
 mapsme_process.process()
-print 'Processing notes'
-parse_notes.process_notes()
