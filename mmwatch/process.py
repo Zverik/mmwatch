3afa3cc1323e127a18301a46a3ed17f780c1e20b
Ilya Zverev <zverik@textual.ru>
2016-06-09 17:03:52 +0300

    Process.py to work without virtualenv

--- mmwatch/process.py
+++ mmwatch/process.py
@@ -1,3 +1,11 @@
 #!/usr/bin/env python
+import os, sys
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+sys.path.insert(0, BASE_DIR)
+PYTHON = 'python2.7'
+VENV_DIR = os.path.join(BASE_DIR, 'venv', 'lib', PYTHON, 'site-packages')
+if os.path.exists(VENV_DIR):
+    sys.path.insert(1, VENV_DIR)
+
 from server import mapsme_process
 mapsme_process.process()
