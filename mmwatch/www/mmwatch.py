45e59d6b70558f3a5a6d9b98c3e725465d8567f7
Ilya Zverev <zverik@textual.ru>
2017-01-26 14:45:58 +0300

    Oops, forgot to use query string for caching

--- mmwatch/www/mmwatch.py
+++ mmwatch/www/mmwatch.py
@@ -15,7 +15,7 @@ def cached(timeout=5 * 60, key='view'):
     def decorator(f):
         @wraps(f)
         def decorated_function(*args, **kwargs):
-            cache_key = '{}/{}'.format(key, request.path)
+            cache_key = '{}/{}'.format(key, request.full_path)
             rv = cache.get(cache_key)
             if rv is not None:
                 return rv
